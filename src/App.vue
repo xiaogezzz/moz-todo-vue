79f19b2b9f5ecbec7a34ea0f0764f08806cacb4a
Shawn-newteng <844699335@qq.com>
2021-04-15 18:39:31 +0800

    feat: 初步实践

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My First Awesome Item" :done="true"></to-do-item>
+      </li>
+      <li>
+        <to-do-item label="My Second Item" :done="false"></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ToDoItem,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input type="checkbox" name="" :id="id" :checked="isDone" />
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+<script>
+import uniqueId from "lodash.uniqueid";
+
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+  },
+  data() {
+    return {
+      isDone: this.done,
+      id: uniqueId("todo-"),
+    };
+  },
+};
+</script>
